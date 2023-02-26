61425a306bf9fc42c97204a33d5476750b9fa00c
César Bruzual <cesarbruzual89@gmail.com>
2023-02-25 20:07:52 -0400

    fix layout

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, redirect, render_template, request, url_for
+from flask import Flask, render_template, url_for
 import requests
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def home():
     response = requests.get(url) 
     r_json = response.json()
     personajes = r_json['results']
-    return render_template('index.html', data = personajes)
+    return render_template('index.html', data=personajes)
 
 if __name__ == "__main__":
     app.run(debug=True)
